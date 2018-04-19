f0488f5cb1331978096d603a837edf67f8879d05
Max Pederson <32686130+mvp3py@users.noreply.github.com>
2018-04-18 20:04:25 -0400

    Update ingredients table and script

--- Data/Ingredients/Ingredients_Scraping.py
+++ Data/Ingredients/Ingredients_Scraping.py
@@ -1,6 +1,7 @@
 from lxml import html
 import requests
 import pandas as pd
+import numpy as np
 
 # Load webpage
 page = requests.get('https://www.paulaschoice.com/ingredient-dictionary')
@@ -10,10 +11,36 @@ tree = html.fromstring(page.content)
 
 # Pull ingredients
 ingredients = tree.xpath('//*[@id="primary"]/div[2]/div/div[2]/div/table/tbody//td[2]/h2/a/text()')
+ratings = tree.xpath('//*[@id="primary"]/div[2]/div/div[2]/div/table/tbody//td[1]/text()')
+
+listy = []
+for i in range(1715):
+    list1 = tree.xpath('//*[@id="primary"]/div[2]/div/div[2]/div/table/tbody/tr[{}]/td[2]/p/text()'.format(i))
+    listy.append(list1)
+    
+#categories = tree.xpath('')
 
 # Write the ingredients to a dataframe
 df = pd.DataFrame(index = ingredients)
+df = pd.DataFrame({'Ingredient': ingredients, 'Rating': ratings, 'Description': listy})
 
-# Put in CSV, append each time it updates
-df.to_csv("ingredients_list.csv", mode = 'a', header = False)    
-   
+
+
+# Clean dataframe
+df['Description'] = df['Description'].astype(str)
+df['Description'] = df['Description'].shift(-1)
+df = df.replace('[]', '')
+df = df.replace(np.NaN, '')
+
+listy2 = []
+for i in df['Description']:
+    listy2.append(i.replace("['\\r\\n\\t\\t\\t\\t                \\t",'').
+                  replace("\\r\\n\\t\\t\\t\\t                ']","").replace("',","").replace("']","").replace(
+    '["\\r\\n\\t\\t\\t\\t                \\t',"").replace('\\r\\n\\t\\t\\t\\t                "',"").replace("[","").replace("]",""))
+
+df['Description'] = listy2    
+df = df[['Ingredient','Rating','Description']]
+
+# Put in CSV, append each time it updates   
+df.to_csv("ingredients.csv", mode = 'a', index = False)
+
